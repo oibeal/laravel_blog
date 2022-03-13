3e23ee798ec098c6d32b554583ddb1573587b069
obeaskoetxea <obeaskoetxea@gmail.com>
2022-03-13 16:25:21 +0100

    Viendo la plantilla de jetstream

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Laravel Blog') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <!-- <x-jet-welcome /> -->
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia saepe necessitatibus eius! Provident dolore rerum debitis vel reiciendis. Deserunt similique sed repudiandae odio soluta quibusdam impedit labore nostrum perspiciatis quas.</p>
+                <p>Et iure quibusdam necessitatibus tempora reiciendis optio error dicta voluptate veritatis laboriosam perspiciatis officia quos molestiae, voluptas blanditiis, deleniti eveniet eligendi sapiente consequatur alias. Architecto laudantium eaque atque dolor at.</p>
+                <p>Architecto excepturi, eum eius libero dignissimos ab distinctio explicabo ratione, maxime nobis incidunt! Cupiditate ullam officiis ipsam non eum reprehenderit velit. Quidem nesciunt neque maiores ratione sed illum ducimus necessitatibus?</p>
+                <p>Libero, eum non, ut ad voluptatibus eligendi veniam adipisci aspernatur doloribus quis officiis. Cumque ex ducimus ad similique natus, eligendi distinctio, labore optio pariatur culpa et at, maxime repellat voluptas.</p>
+                <p>Blanditiis molestias omnis obcaecati laudantium quisquam maxime reiciendis. Quos consequuntur, dicta facilis doloremque culpa rerum a officia quae ab sapiente suscipit repudiandae omnis. Culpa esse mollitia quis quas iusto aut.</p>
             </div>
         </div>
     </div>
